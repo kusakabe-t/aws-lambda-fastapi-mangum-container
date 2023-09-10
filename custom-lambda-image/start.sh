6124683e48ecd51858efd5e895bdbc1ceba91f45
kusakabe <kusakabe.takuya@alleeks.com>
2023-09-10 19:48:52 +0900

    refactoring

--- custom-lambda-image/start.sh
+++ custom-lambda-image/start.sh
@@ -2,5 +2,5 @@
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
   docker run -v ~/.aws-lambda-rie:/aws-lambda -p 9000:8080 --entrypoint /aws-lambda/aws-lambda-rie fastapi-mangum-custom-container-sample /usr/local/bin/python -m awslambdaric app.handler
 else
-  echo "please install aws-lambda-rie. \ncf. https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/images-test.html#images-test-add"
+  echo "please install aws-lambda-rie. \ncf. https://github.com/aws/aws-lambda-python-runtime-interface-client#local-testing"
 fi
